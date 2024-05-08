02b592ca53fca14ed658af31b2333682eec9d005
VaibhavDhamanage <vaibhavdhamanage>
2024-05-08 18:57:59 +0530

    Build Demo Project. create form and show input.

---
+++ Demo_1/app.js
@@ -0,0 +1,58 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  const body = [];
+  if (url === "/") {
+    fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      res.write("<html>");
+      res.write("<head><title>Enter Message</title></head>");
+      res.write(`<body><P>${data}</P></body>`);
+      console.log(data);
+      res.write("<body>");
+      res.write(
+        `<form action="/message" method="POST"><input type='text' name='message'><button type="submit">Submit</button></form>`
+      );
+      res.write("</body>");
+      res.write("</html>");
+      return res.end();
+    });
+  } else if (url === "/message" && method === "POST") {
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parse = Buffer.concat(body).toString();
+      const message = parse.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(fs.writeFile);
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+
+  // Handling GET requests
+  else {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Pages</title></head>");
+    res.write("<body><h1>My First Pages</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
