c59fee6596279fce24857b8c28ae7fbbcb14fe34
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-05-10 09:11:07 +0530

    add nodemon and express and create server using express and use middleware

--- Demo_1/app.js
+++ Demo_1/app.js
@@ -1,9 +1,15 @@
 const http = require("http");
+const express = require("express");
 
 const routes = require("./routes");
 
-const server = http.createServer(routes);
+// const server = http.createServer(routes);
+const app = express(routes);
 
-server.listen(3000, () => {
+app.use((req, res) => {
+  res.send("<h1> Vaibhav Dhamanage </h1>");
+});
+
+app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
