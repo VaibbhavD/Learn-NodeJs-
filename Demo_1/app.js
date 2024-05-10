b995f144e54844f386edee33938cf623fae98a3a
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-05-10 10:03:57 +0530

    add routes

--- Demo_1/app.js
+++ Demo_1/app.js
@@ -1,13 +1,20 @@
-const http = require("http");
 const express = require("express");
-
+const blog = require("./blog.js");
 const routes = require("./routes");
 
 // const server = http.createServer(routes);
-const app = express(routes);
+const app = express();
+// app.use(express.static("static"));
+app.use("/blog", blog);
 
-app.use((req, res) => {
-  res.send("<h1> Vaibhav Dhamanage </h1>");
+// app.use((req, res) => {
+//   res.send("<h1> Vaibhav Dhamanage </h1>");
+// });
+app.get("/", (req, res) => {
+  res.send("<h1>Welocme to My Home Page</h1>");
+});
+app.get("/:slug", (req, res) => {
+  res.send(`<h1>Welocme to My ${req.params.slug} Page</h1>`);
 });
 
 app.listen(3000, () => {
