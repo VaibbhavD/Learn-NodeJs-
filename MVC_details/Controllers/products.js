a21f4feff30c9debe350fc63752bfe9335ffffc9
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-07-26 18:11:11 +0530

    Add Model for Product

--- MVC_details/Controllers/products.js
+++ MVC_details/Controllers/products.js
@@ -1,27 +1,28 @@
-const products = [];
-
+const Products = require("../models/product");
 exports.getAddProduct = (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
     formsCSS: true,
     productCSS: true,
-    activeAddProduct: true
+    activeAddProduct: true,
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
+  const product = new Products(req.body.title);
+  product.save();
+  res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
-  res.render('shop', {
+  const products = Products.fetchAll();
+  res.render("shop", {
     prods: products,
-    pageTitle: 'Shop',
-    path: '/',
+    pageTitle: "Shop",
+    path: "/",
     hasProducts: products.length > 0,
     activeShop: true,
-    productCSS: true
+    productCSS: true,
   });
 };
